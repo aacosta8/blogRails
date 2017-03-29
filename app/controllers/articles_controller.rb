c57725343939c729987a08ba9abf897fd6511567
Alex <aacosta@eafit.edu.co>
2017-03-29 09:56:58 -0500

    Uso de before_action (callbacks) y modelos

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,6 @@
 class ArticlesController < ApplicationController
-
+	before_action :authenticate_user!, except: [:show, :index]
+	before_action :set_article, except: [:index,:new,:create]
 	#get "/articles"
 	def index
 		@articles = Article.all
@@ -7,7 +8,7 @@ class ArticlesController < ApplicationController
 
 	#get "/articles/:id"
 	def show
-		@article = Article.find(params[:id])
+		@article.update_visits_count
 	end
 
 	#get "/articles/new"
@@ -16,7 +17,6 @@ class ArticlesController < ApplicationController
 	end
 
 	def edit
-		@article = Article.find(params[:id])
 	end
 
 	#post "/articles"
@@ -31,7 +31,6 @@ class ArticlesController < ApplicationController
 
 	#put "articles/:id"
 	def update
-		@article = Article.find(params[:id])
 		if @article.update(article_params)
 			redirect_to @article
 		else
@@ -40,12 +39,15 @@ class ArticlesController < ApplicationController
 	end
 
 	def destroy
-		@article = Article.find(params[:id])
 		@article.destroy
 		redirect_to articles_path
 	end
 
 	private
+
+	def set_article
+		@article = Article.find(params[:id])
+	end
 
 	def article_params
 		params.require(:article).permit(:title,:body)
