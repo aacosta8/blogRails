8ee30c00d06ba911055b6345de08b37ee3fb740a
Alex <aacosta@eafit.edu.co>
2017-04-30 19:57:34 -0500

    Deploy Message

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'allexx-20-12@hotmail.com'
+  default from: 'from@example.com'
   layout 'mailer'
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  default_url_options :host => "example.com"
   resources :articles do
-    resources :comments, only: [:create, :update, :destroy] 
+    resources :comments, only: [:create, :update, :destroy]
   end
   devise_for :users
   root 'welcome#index'
