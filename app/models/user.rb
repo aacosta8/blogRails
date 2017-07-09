0d73e7fe39d1f2941fd58c24a332db29c3a555b9
Alex <aacosta@eafit.edu.co>
2017-07-09 01:41:48 -0500

    Mejorando las vistas(Articulo individual) y utilizando gravatar

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,18 @@ class User < ApplicationRecord
   has_many :articles
   has_many :comments
   include PermissionsConcern
+
+  def avatar
+    # include MD5 gem, should be part of standard ruby install
+    require 'digest/md5'
+
+    # get the email from URL-parameters or what have you and make lowercase
+    email_address = self.email.downcase
+
+    # create the md5 hash
+    hash = Digest::MD5.hexdigest(email_address)
+
+    # compile URL which can be used in <img src="RIGHT_HERE"...
+    image_src = "https://www.gravatar.com/avatar/#{hash}"
+  end
 end
