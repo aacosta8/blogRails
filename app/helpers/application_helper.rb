6c4af8b57146e173565c66716e1bec40be615418
Alex <aacosta@eafit.edu.co>
2017-07-09 22:03:30 -0500

    Application helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+
+  def article_cover url, options = {}
+
+    html_class = options[:class]
+    html_style = "background:url(#{url});"\
+                 "width:100%;height:400px;background-size: cover;"
+
+    html = "<header class='#{html_class}' style='#{html_style}'>"\
+           "</header>"
+
+    html.html_safe
+  end
+
 end
